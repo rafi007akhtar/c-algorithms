6a16ff131c633933796310c046939434b3aba42d
Md Rafi Akhtar <alimdrafi@gmail.com>
2017-11-05 14:08:24 +0530

    Backtracking program 1: N-Queens

---
+++ nqueens.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// create bool datatype
+enum bool {false, true}; 
+typedef enum bool bool;
+
+// inline functions
+#define write(a, n)  // print out an array 'a' of length n; 'i' needs to be initialized in the function
+
+// user-defined functions
+bool place(int k, int i); // returns true (1) if a queen can be placed on kth row and ith column; false (0) otherwise
+void nqueens(int k, int n); // print the backtracking soln to this NQueens problem
+
+int *x; // solution to the backtracking problem (NQueens)
+
+int main()
+{
+	int n;
+	printf("Enter the value of N: ");
+	scanf("%d", &n);
+	
+	x = (int *)malloc(n * sizeof(int));
+	nqueens(0, n);
+	
+	return 0;
+}
+
+bool place(int k, int i)
+{
+	int j;
+	for (j = 0;  j < k; j++)
+	{
+		// column check
+		if (x[j] == i) return false;
+		
+		// diagonal check: difference in ordinates should not equal difference in abscissae
+		if (abs(x[j]-i) == abs(j-k)) return false;
+	}
+	return true;
+}
+
+void nqueens(int k, int n)
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+	{
+		if (place(k, i)) // if kth row can be placed on ith column
+		{
+			//printf("True for (%d, %d)\n", k, i);
+			x[k] = i;
+			if (k == n-1)
+			{
+				for (j = 0; j < n; j++) 
+					printf("%d ", x[j]);
+				//exit(0);
+				printf("\n");
+			}
+			else nqueens(k+1, n);
+		}
+	}
+}
